b0bbd00f02b9af592416b7bdcc4182f04be59fed
Paul Nizet <polonizet@gmail.com>
2022-09-09 15:48:10 +0200

    finish homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import beerImage from "../assets/beers.png"
+import newBeerImage from "../assets/new-beer.png"
+import randomBeerImage from "../assets/random-beer.png"
 
 const HomePage = () => {
     return (
@@ -10,6 +12,26 @@ const HomePage = () => {
                 <h2>All Beers</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam auctor, nisl eget aliquam tincidunt, nunc nisl
+                    tincidunt nisl, nec aliquam nisl nisl sit amet lorem.
+                </p>
+            </div>
+            <div>
+                <img src={randomBeerImage} alt="randomBeerImage" />
+                <h2>Random Beers</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam auctor, nisl eget aliquam tincidunt, nunc nisl
+                    tincidunt nisl, nec aliquam nisl nisl sit amet lorem.
+                </p>
+            </div>
+            <div>
+                <img src={newBeerImage} alt="newBeerImage" />
+                <h2>New beers</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam auctor, nisl eget aliquam tincidunt, nunc nisl
+                    tincidunt nisl, nec aliquam nisl nisl sit amet lorem.
                 </p>
             </div>
         </div>
